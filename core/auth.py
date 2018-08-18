2aa34d157ff0815ff041a3fece8801fb8e381200
Douglas Francisco Cardoso <douglasfrancardoso@gmail.com>
2018-08-17 21:05:31 -0300

    Check login no database sqlite

--- core/auth.py
+++ core/auth.py
@@ -1,8 +1,20 @@
 from flask import session
+from core.db import db
 
 
 def check_login(username, password):
-    if(username == "" and password == ""):
+    count = db().execute('SELECT count(*) as qtd FROM user').fetchone()
+
+    if count["qtd"] == 0:
+        db().execute('INSERT INTO user (username, password) VALUES (?,?)', ('admin','admin'))
+        db().commit()
+
+    user = db().execute('SELECT password FROM user where username = ?', (username, )).fetchone()
+
+    if not user:
+        return False
+
+    if(password == user["password"]):
         return True
     else:
         return True
--- webrpi/__init__.py
+++ webrpi/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from core import db
 
 app = Flask(__name__)
 
 app.secret_key = b'H^a/$-re8SRJ9kZe*(u)u/%{3H~Z\\\O'
+
+db.init_app(app)
 
 import modules.hardware.so
 import webrpi.views
